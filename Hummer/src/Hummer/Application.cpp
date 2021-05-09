1d49d2616957a572a402998955282c148b417fd7
realjf <real_jf@hotmail.com>
2021-05-09 14:17:22 +0800

    update

--- Hummer/src/Hummer/Application.cpp
+++ Hummer/src/Hummer/Application.cpp
@@ -13,6 +13,40 @@ namespace Hummer {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Hummer::ShaderDataType::None:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Float:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Float2:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Float3:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Float4:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Mat3:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Mat4:
+			return GL_FLOAT;
+		case Hummer::ShaderDataType::Int:
+			return GL_INT;
+		case Hummer::ShaderDataType::Int2:
+			return GL_INT;
+		case Hummer::ShaderDataType::Int3:
+			return GL_INT;
+		case Hummer::ShaderDataType::Int4:
+			return GL_INT;
+		case Hummer::ShaderDataType::Bool:
+			return GL_BOOL;
+		}
+
+		HM_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		HM_CORE_ASSERT(!s_Instance, "Applicaton already exists!");
@@ -26,17 +60,40 @@ namespace Hummer {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" },
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
+		
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(
+				index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
+		
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const void*)12);
 
 		unsigned int indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices)/ sizeof(uint32_t)));
@@ -45,12 +102,15 @@ namespace Hummer {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -61,10 +121,12 @@ namespace Hummer {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
--- Hummer/src/Platform/OpenGL/OpenGLBuffer.h
+++ Hummer/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,11 +10,17 @@ namespace Hummer {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void UnBind() const;
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
+
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+		virtual const BufferLayout& GetLayout() const override {
+			return m_Layout;
+		}
 
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
