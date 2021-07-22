6fe861075e36193fe7f9a4399d39e92a8b3b91c4
realjf <real_jf@hotmail.com>
2021-07-22 19:27:43 +0800

    update

--- Hummernut/src/EditorLayer.cpp
+++ Hummernut/src/EditorLayer.cpp
@@ -67,8 +67,10 @@ namespace Hummer {
 		HM_PROFILE_FUNCTION();
 
 		// Resize	
-		if (FramebufferSpecification spec = m_Framebuffer->GetSpecification(); m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f &&
-			(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+		if (FramebufferSpecification spec = m_Framebuffer->GetSpecification(); 
+			m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f
+			//&& (spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y)
+			)
 		{
 			HM_ERROR("{0},{1}", spec.Width, m_ViewportSize.x);
 			m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
@@ -211,13 +213,14 @@ namespace Hummer {
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-		if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize) && viewportPanelSize.x > 0 && viewportPanelSize.y > 0)
+		m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
+		/*if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize) && viewportPanelSize.x > 0 && viewportPanelSize.y > 0)
 		{
 			m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
 			m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 
 			m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-		}
+		}*/
 		HM_WARN("Viewport Size: {0}, {1}", viewportPanelSize.x, viewportPanelSize.y);
 		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererID();
 		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
