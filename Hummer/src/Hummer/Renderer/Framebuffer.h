f82a5bd9bc7697cbf231ee623536cbf2ecad346d
realjf <real_jf@hotmail.com>
2021-07-28 21:01:51 +0800

    update

--- Hummer/src/Hummer/Renderer/Framebuffer.h
+++ Hummer/src/Hummer/Renderer/Framebuffer.h
@@ -2,10 +2,44 @@
 
 
 namespace Hummer {
+
+	enum class FramebufferTextureFormat
+	{
+		None = 0,
+
+		//Color
+		RGBA8,
+
+
+		// Depth/stencil
+		DEPTH24STENCIL8,
+
+		// Defaults
+		Depth = DEPTH24STENCIL8
+	};
+
+	struct FramebufferTextureSpecification
+	{
+		FramebufferTextureSpecification() = default;
+		FramebufferTextureSpecification(FramebufferTextureFormat format)
+			: TextureFormat(format) {}
+
+		FramebufferTextureFormat TextureFormat;
+	};
+
+	struct FramebufferAttachmentSpecification
+	{
+		FramebufferAttachmentSpecification() = default;
+		FramebufferAttachmentSpecification(std::initializer_list<FramebufferTextureSpecification> attachments)
+			: Attachments(attachments) {}
+
+		std::vector<FramebufferTextureSpecification> Attachments;
+	};
+
 	struct FramebufferSpecification
 	{
 		uint32_t Width = 0, Height = 0;
-		
+		FramebufferAttachmentSpecification Attachments;
 		uint32_t Samples = 1;
 
 		bool SwapChainTarget = false;
